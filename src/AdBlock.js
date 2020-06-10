24b9e5f4356467c30b3550f9ea94241a084220b8
Abbas Mokhtari <abs.mokhtari@gmail.com>
2020-06-10 14:07:06 +0100

    Heading Finished

---
+++ src/AdBlock.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdBlock() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AdBlock
---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import header from './images/bg-header-desktop.svg'
+
+function Heading() {
+    return (
+        <div>
+        <img src={header} alt="This is a banner" className='headerImg'/>
+        </div>
+    )
+}
+
+export default Heading
